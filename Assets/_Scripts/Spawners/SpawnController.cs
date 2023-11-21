e009e0b75770a442a5e881a9240c62c9039d6cf3
Tor-Arne Sandstrak <togo.danderfluff@gmail.com>
2023-11-22 00:43:29 +0100

    Added SpawnController to Arena

---
+++ Assets/_Scripts/Spawners/SpawnController.cs
@@ -0,0 +1,46 @@
+using System.Collections.Generic;
+using Unity.Netcode;
+using UnityEngine;
+
+public class SpawnController : MonoBehaviour
+{
+	private List<Transform> _spawnLocations = new(); 
+	private int _spawnIndex = 0;
+	private bool _nextSpawnIsRed = true;
+	private TeamColor _teamColor;
+
+	private void Awake () {
+		foreach (Transform t in transform) {  
+			_spawnLocations.Add(t); 
+		}
+	}
+
+	private void IncreaseSpawnIndex() {
+		_spawnIndex = (_spawnIndex + 1 < _spawnLocations.Count ) ? _spawnIndex + 1: 0;
+	}
+
+	public void SpawnPlayer(GameObject _playerPrefab) {
+		Vector2 _spawnLocation = _spawnLocations[_spawnIndex].position;
+		
+		if ( _nextSpawnIsRed ) {
+			_teamColor = TeamColor.RED;
+			_nextSpawnIsRed = !_nextSpawnIsRed;
+
+		} else { 
+			_teamColor = TeamColor.BLUE;
+			_nextSpawnIsRed = !_nextSpawnIsRed;
+
+		}
+
+		GameObject _gameObject = Instantiate( _playerPrefab, _spawnLocation, Quaternion.identity );
+		PlayerController _playerController = _gameObject.GetComponent<PlayerController>();
+		_playerController.AssignTeam(_teamColor);
+
+		if (_gameObject.TryGetComponent( out NetworkObject netObject)) {
+			netObject.Spawn();
+
+		}
+
+		IncreaseSpawnIndex();
+	}
+}
