8bad68cafba23077a0a0f615a7cb3f28455a8e07
Tor-Arne Sandstrak <togo.danderfluff@gmail.com>
2023-11-22 00:00:56 +0100

    Added SetPlayersPerTeam function to InterfaceController

--- Assets/_Scripts/InterfaceController.cs
+++ Assets/_Scripts/InterfaceController.cs
@@ -7,9 +7,15 @@ using UnityEngine;
 public class InterfaceController : MonoBehaviour {
 	[SerializeField] private TMP_Text _interfaceJoinCode;
 	[SerializeField] private GameObject _interfaceJoinCodeGroup;
+	[SerializeField] private TMP_Text _interfaceTeamsValue;
+
 
 	public void SetJoinCode ( string joinCode ) {
 		_interfaceJoinCode.text = joinCode;
 		_interfaceJoinCodeGroup.SetActive(true);
+	}
+
+	public void SetPlayersPerTeam(int red = 0, int blue = 0 ) {
+		_interfaceTeamsValue.SetText($"<color=Red>{red}</color> / <color=blue>{blue}</color>" );
 	}
 }
